5a9a092dc4381c466e27f777cee606592d477839
hoangbathang <thangcntt2306@gmail.com>
2022-08-12 05:51:39 +0700

    deploy

---
+++ client/server.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
+const app = express();
+
+let PORT = process.env.PORT || 3000;
+app.use(express.static('build'))
+app.listen(PORT, ()=> {
+  console.log('server running on port '+ PORT);
+})
