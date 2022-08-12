0f45b93b3a25a55653ba9c4df2e102a21942d3e3
hoangbathang <thangcntt2306@gmail.com>
2022-08-12 16:16:32 +0700

    change-server-

--- client/server.js
+++ client/server.js
@@ -7,7 +7,7 @@ let PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build'))); 
 
-app.get('*', function(req, res) {     res.sendFile(path.join(__dirname + '/build/index.html')); });
+
 app.listen(PORT, ()=> {
   console.log('server running on port '+ PORT);
 })
