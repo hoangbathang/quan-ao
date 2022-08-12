1705bf064cc247d0fb1f00e9c617bd8317132e18
hoangbathang <thangcntt2306@gmail.com>
2022-08-12 16:10:14 +0700

    deploy -server

--- client/server.js
+++ client/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 let PORT = process.env.PORT || 3000;
-app.use(express.static('build'));
+
 app.use(express.static(path.join(__dirname, 'build'))); 
 
 app.get('*', function(req, res) {     res.sendFile(path.join(__dirname + '/build/index.html')); });
