b08aa2374b355eb39bbe0416c19dbba3d8597023
hoangbathang <thangcntt2306@gmail.com>
2022-08-12 16:22:37 +0700

    chan-server.js

--- client/server.js
+++ client/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 let PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build'))); 
+app.use(express.static('build')); 
 
 
 app.listen(PORT, ()=> {
